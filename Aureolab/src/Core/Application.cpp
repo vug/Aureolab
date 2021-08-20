4dfc32bba69fb6dca5e7becea9bdedbb5bcfcdf2
Uğur Güney <ugurguney@gmail.com>
2021-08-19 21:47:13 -0400

    Use GLFW to create an example window and draw triangle animation with legacy OpenGL

--- Aureolab/src/Core/Application.cpp
+++ Aureolab/src/Core/Application.cpp
@@ -1,10 +1,49 @@
 #include "Application.h"
 #include "Log.h"
 
+#include <GLFW/glfw3.h>
+
 #include <string>
 
 Application::Application(const std::string& name) : name(name) { }
 
 void Application::Run() {
-	Log::Info("{} is running", name);
+	Log::Info("{} is running. Creating window...", name);
+
+    GLFWwindow* window;
+    glfwInit();
+    window = glfwCreateWindow(640, 480, name.c_str(), NULL, NULL);
+    glfwMakeContextCurrent(window);
+    while (!glfwWindowShouldClose(window)) {
+        float ratio;
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+        ratio = width / (float)height;
+
+        glViewport(0, 0, width, height);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+        glMatrixMode(GL_MODELVIEW);
+
+        glLoadIdentity();
+        glRotatef((float)glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
+
+        glBegin(GL_TRIANGLES);
+        glColor3f(1.f, 0.f, 0.f);
+        glVertex3f(-0.6f, -0.4f, 0.f);
+        glColor3f(0.f, 1.f, 0.f);
+        glVertex3f(0.6f, -0.4f, 0.f);
+        glColor3f(0.f, 0.f, 1.f);
+        glVertex3f(0.f, 0.6f, 0.f);
+        glEnd();
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
 }
