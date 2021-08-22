44ea094fc9b8b5c6bf934aa0ac80969db0bf534d
Uğur Güney <ugurguney@gmail.com>
2021-08-22 17:23:52 -0400

    Update the main Application code to use new Window system
    Now Application layer is not aware of GLFW library. Uses a windowing system via Window API.

--- Aureolab/src/Core/Application.cpp
+++ Aureolab/src/Core/Application.cpp
@@ -3,99 +3,27 @@
 
 #include <string>
 
-static void error_callback(int error, const char* description)
-{
-    Log::Error("Error [{}]: {}", error, description);
-}
-
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
-{
-    auto app = (Application*)glfwGetWindowUserPointer(window);
-    app->OnKeyPress(key, scancode, action, mods);
-}
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-    glViewport(0, 0, width, height);
-}
-
 Application::Application(const std::string& name) : name(name) { 
-    // Window::Initialize
-    if (!glfwInit()) {
-        Log::Critical("Could not initialize GLFW. Exiting...");
-        return;
-    }
-
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-#ifdef IS_MAC_OS // find an actual macro to detect MacOS
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-#endif
-    // other hints: GLFW_OPENGL_DEBUG_CONTEXT, GLFW_COCOA_RETINA_FRAMEBUFFER 
-
-    window = glfwCreateWindow(1000, 1000, name.c_str(), NULL, NULL);
-    if (!window) {
-        glfwTerminate();
-        Log::Critical("Could not initialize GLFW window. Exiting...");
-        return;
-    }
-
-    glfwSetWindowUserPointer(window, this);
-    glfwSetKeyCallback(window, key_callback);
-    glfwSetErrorCallback(error_callback);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-    {
-        // GraphicsContext::Initialize
-        glfwMakeContextCurrent(window);
-        gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
-    }
-
-    glfwSwapInterval(1);  // VSync enabled. (0 for disabling), requires context
-
-    Log::Info("OpenGL Info:");
-    Log::Info("Renderer: {}", glGetString(GL_RENDERER));
-    Log::Info("Vendor: {}", glGetString(GL_VENDOR));
-    Log::Info("Version: {}", glGetString(GL_VERSION));
+    window = Window::CreateAndInitialize(name, 1000, 1000);
 
     // Renderer::Initialize
     // TODO: Enable GL debugging, other glEnable (blending, blend function, depth test etc) to application defaults
 }
 
 void Application::Run() {
-	Log::Info("{} is running. Creating window...", name);
+	Log::Info("{} ", name);
 
-    while (!glfwWindowShouldClose(window)) {
+    while (window->IsRunning()) {
         for (auto layer : layers) {
             layer->OnUpdate(0.1f);
         }
-
-        // Window::OnUpdate
-        glfwPollEvents();
-        {
-            // GraphicsContext
-            glfwSwapBuffers(window);
-        }
+        window->OnUpdate();
     }
 
     for (int i = 0; i < layers.size(); i++) {
         PopLayer();
     }
 
-    // Window::Shutdown
-    glfwDestroyWindow(window);
-    glfwTerminate();
-}
-
-void Application::Close() {
-    glfwSetWindowShouldClose(window, GLFW_TRUE);
-}
-
-void Application::OnKeyPress(int key, int scancode, int action, int mods) {
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        Close();
+    window->Shutdown();
+    delete window;
 }
--- Aureolab/src/Core/Application.h
+++ Aureolab/src/Core/Application.h
@@ -1,10 +1,7 @@
 #pragma once
 
 #include "LayerList.h"
-
-#define GLFW_INCLUDE_NONE
-#include <GLFW/glfw3.h>
-#include <glad/glad.h>
+#include "Window.h"
 
 #include <memory>
 #include <string>
@@ -12,12 +9,9 @@
 class Application : public LayerList {
 public:
 	Application(const std::string& name);
-	GLFWwindow* GetWindow() { return window; }
-	void OnKeyPress(int key, int scancode, int action, int mods);
-	void Close();
 private:
 	void Run();
-	GLFWwindow* window = nullptr;
+	Window* window = nullptr;
 	std::string name;
 	// Applications can be created by client apps but can only be ran from EntryPoint's main
 	friend int main(int argc, char* argv[]);
