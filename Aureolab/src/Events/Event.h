caef5e71439de745fa623383d9fbdf6a49261f22
Uğur Güney <ugurguney@gmail.com>
2021-08-28 16:21:02 -0400

    Start Event system with Event and Dispatcher classes.
    * Base abstract Event just asks to implement a GetName.
      * default ToString is just event's name.
      * overload << for easier printing out of events
    * Dispatcher first wraps incoming base event.
      * then sends them to various handler methods on the same class.
      * for each target handler a new Dispatch call is needed.
      * Dispatcher only dispatches/sends events if received event is the same type as target handler's event type.
    * First two Window events as example.

---
+++ Aureolab/src/Events/Event.h
@@ -0,0 +1,47 @@
+#pragma once
+#include <functional>
+#include <iostream>
+#include <string>
+#include <type_traits>
+
+//#define AL_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
+// black magic that replaces above bind with lambda function.
+#define AL_BIND_EVENT_FN(fn) [this](auto&&... args) -> decltype(auto) { return this->fn(std::forward<decltype(args)>(args)...); }
+
+class Event {
+public:
+	virtual ~Event() = default;
+
+	// bool handled = false; // TODO: Implement for stopping spread of an event. Say, after top layer handles the event, lower layers won't receive it.
+
+	virtual const char* GetName() const = 0;
+	virtual std::string ToString() const { return GetName(); }
+};
+
+inline std::ostream& operator<<(std::ostream& os, const Event& e)
+{
+	return os << e.ToString();
+}
+
+
+class EventDispatcher {
+public:
+	EventDispatcher(Event& event)
+		: event(event) {}
+
+	template<typename TEvent> // A Concrete Event class
+	bool Dispatch(std::function<bool(TEvent&)> func) {
+		static_assert(std::is_base_of_v<Event, TEvent>);
+
+		// Check whether incoming concrete event type is the target type of this Dispatch call
+		if (typeid(TEvent) == typeid(event)) { // Reference to event cannot be dynamic_cast'ed safely
+			func(static_cast<TEvent&>(event));
+			return true;
+		}
+		else { // Types mismatch, not dispatching/sending.
+			return false;
+		}
+	}
+private:
+	Event& event;
+};
---
+++ Aureolab/src/Events/WindowEvent.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "Event.h"
+
+#include <sstream>
+
+class WindowResizeEvent : public Event {
+public:
+	WindowResizeEvent(unsigned int width, unsigned int height)
+		: width(width), height(height) {}
+
+	virtual const char* GetName() const override { return "WindowResizeEvent"; }
+
+	unsigned int GetWidth() const { return width; }
+	unsigned int GetHeight() const { return height; }
+
+	std::string ToString() const override {
+		std::stringstream ss;
+		ss << GetName() << ": " << width << ", " << height;
+		return ss.str();
+	}
+private:
+	unsigned int width, height;
+};
+
+class WindowCloseEvent : public Event {
+public:
+	WindowCloseEvent() {};
+
+	virtual const char* GetName() const override { return "WindowCloseEvent"; }
+
+	std::string ToString() const override {
+		return GetName();
+	}
+
+};
