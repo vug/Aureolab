ce06c996d97a7cc142fdf6371ef9804168231eb9
Uğur Güney <ugurguney@gmail.com>
2021-08-28 20:34:48 -0400

    [DependencyTestBed] simple GLFW app with legacy OpenGL

--- DependencyTestBed/DependencyTestBed.cpp
+++ DependencyTestBed/DependencyTestBed.cpp
@@ -1,5 +1,6 @@
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
+#include <GLFW/glfw3.h>
 
 #include <memory>
 
@@ -17,5 +18,42 @@ std::shared_ptr<spdlog::logger> CreateLogger() {
 int main(int argc, char* argv[]) {
 	auto logger = CreateLogger();
 	logger->info("Hi from Dependency Test Bed!");
+
+    GLFWwindow* window;
+    glfwInit();
+    window = glfwCreateWindow(1024, 768, "Dependency Test Bed", NULL, NULL);
+    glfwMakeContextCurrent(window);
+    while (!glfwWindowShouldClose(window)) {
+        float ratio;
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+        ratio = width / (float)height;
+
+        glViewport(0, 0, width, height);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+        glMatrixMode(GL_MODELVIEW);
+
+        glLoadIdentity();
+        glRotatef((float)glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
+
+        glBegin(GL_TRIANGLES);
+        glColor3f(1.f, 0.f, 0.f);
+        glVertex3f(-0.6f, -0.4f, 0.f);
+        glColor3f(0.f, 1.f, 0.f);
+        glVertex3f(0.6f, -0.4f, 0.f);
+        glColor3f(0.f, 0.f, 1.f);
+        glVertex3f(0.f, 0.6f, 0.f);
+        glEnd();
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
 	return 0;
 }
