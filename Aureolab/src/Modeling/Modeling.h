c45487c1f7237a75682352d3bebc5d3c8052744a
vug <ugurguney@gmail.com>
2021-09-19 06:28:21 -0400

    Vertex attribute specification for BasicVertex. Indices are given as fixed values.
    Also initialize BasicVertex' all fields.

--- Aureolab/src/Modeling/Modeling.h
+++ Aureolab/src/Modeling/Modeling.h
@@ -8,10 +8,18 @@
 #include <vector>
 
 struct BasicVertex {
-    glm::vec3 position;
-    glm::vec3 normal;
-    glm::vec2 texCoord;
+    glm::vec3 position = { 0.0f, 0.0f, 0.0f };
+    glm::vec3 normal = { 0.0f, 0.0f, 0.0f };
+    glm::vec2 texCoord = { 0.0f, 0.0f };
     glm::vec4 color = { 1.0f, 1.0f, 1.0f, 1.0f };
+};
+
+// To be in sync with BasicShader
+static std::vector<VertexAttributeSpecification> BasicVertexAttributeSpecs = {
+    VertexAttributeSpecification{ 0, VertexAttributeSemantic::Position, VertexAttributeType::float32, 3, false},
+    VertexAttributeSpecification{ 1, VertexAttributeSemantic::Normal, VertexAttributeType::float32, 3, false},
+    VertexAttributeSpecification{ 2, VertexAttributeSemantic::UV, VertexAttributeType::float32, 2, false},
+    VertexAttributeSpecification{ 3, VertexAttributeSemantic::Color, VertexAttributeType::float32, 4, false},
 };
 
 std::vector<BasicVertex> LoadOBJ(const std::string& filepath);
