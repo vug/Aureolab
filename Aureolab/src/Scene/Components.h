6cf0ed4cecba8c0876adb84ae97dfe1b4ecb0462
Uğur Güney <ugurguney@gmail.com>
2021-09-30 01:07:18 -0400

    Serialization for glm vector stuff and Tag, Transform components.

--- Aureolab/src/Scene/Components.h
+++ Aureolab/src/Scene/Components.h
@@ -2,6 +2,36 @@
 
 #include <glm/glm.hpp>
 
+#include <string>
+
+namespace glm
+{
+
+	template<class Archive> void serialize(Archive& archive, glm::vec2& v) { archive(v.x, v.y); }
+	template<class Archive> void serialize(Archive& archive, glm::vec3& v) { archive(v.x, v.y, v.z); }
+	template<class Archive> void serialize(Archive& archive, glm::vec4& v) { archive(v.x, v.y, v.z, v.w); }
+	template<class Archive> void serialize(Archive& archive, glm::ivec2& v) { archive(v.x, v.y); }
+	template<class Archive> void serialize(Archive& archive, glm::ivec3& v) { archive(v.x, v.y, v.z); }
+	template<class Archive> void serialize(Archive& archive, glm::ivec4& v) { archive(v.x, v.y, v.z, v.w); }
+	template<class Archive> void serialize(Archive& archive, glm::uvec2& v) { archive(v.x, v.y); }
+	template<class Archive> void serialize(Archive& archive, glm::uvec3& v) { archive(v.x, v.y, v.z); }
+	template<class Archive> void serialize(Archive& archive, glm::uvec4& v) { archive(v.x, v.y, v.z, v.w); }
+	template<class Archive> void serialize(Archive& archive, glm::dvec2& v) { archive(v.x, v.y); }
+	template<class Archive> void serialize(Archive& archive, glm::dvec3& v) { archive(v.x, v.y, v.z); }
+	template<class Archive> void serialize(Archive& archive, glm::dvec4& v) { archive(v.x, v.y, v.z, v.w); }
+
+	// glm matrices serialization
+	template<class Archive> void serialize(Archive& archive, glm::mat2& m) { archive(m[0], m[1]); }
+	template<class Archive> void serialize(Archive& archive, glm::dmat2& m) { archive(m[0], m[1]); }
+	template<class Archive> void serialize(Archive& archive, glm::mat3& m) { archive(m[0], m[1], m[2]); }
+	template<class Archive> void serialize(Archive& archive, glm::mat4& m) { archive(m[0], m[1], m[2], m[3]); }
+	template<class Archive> void serialize(Archive& archive, glm::dmat4& m) { archive(m[0], m[1], m[2], m[3]); }
+
+	//template<class Archive> void serialize(Archive& archive, glm::quat& q) { archive(q.x, q.y, q.z, q.w); }
+	//template<class Archive> void serialize(Archive& archive, glm::dquat& q) { archive(q.x, q.y, q.z, q.w); }
+
+}
+
 struct TagComponent {
 	std::string Tag;
 
@@ -10,6 +40,8 @@ struct TagComponent {
 	TagComponent(const std::string& tag) :
 		Tag(tag) {}
 
+	template <class Archive>
+	void serialize(Archive& ar) { ar(Tag); }
 };
 
 struct TransformComponent {
@@ -19,6 +51,9 @@ struct TransformComponent {
 
 	TransformComponent() = default;
 	TransformComponent(const TransformComponent&) = default;
-	TransformComponent(const glm::vec3& translation) :
-		Translation(translation) {}
+	TransformComponent(const glm::vec3& translation, const glm::vec3& rotation, const glm::vec3& scale) :
+		Translation(translation), Rotation(rotation), Scale(scale) {}
+
+	template <class Archive>
+	void serialize(Archive& ar) { ar(Translation, Rotation, Scale); }
 };
