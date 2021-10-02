02a20f2b90b48d228ee935d9e38598e27f560ca7
Uğur Güney <ugurguney@gmail.com>
2021-10-02 09:43:20 -0400

    include glm quaternions for serialization

--- Aureolab/src/Scene/Components.h
+++ Aureolab/src/Scene/Components.h
@@ -1,12 +1,12 @@
 #pragma once
 
 #include <glm/glm.hpp>
+#include<glm/gtc/quaternion.hpp>
 
 #include <string>
 
-namespace glm
-{
-
+// For serialization of glm data structures
+namespace glm {
 	template<class Archive> void serialize(Archive& archive, glm::vec2& v) { archive(v.x, v.y); }
 	template<class Archive> void serialize(Archive& archive, glm::vec3& v) { archive(v.x, v.y, v.z); }
 	template<class Archive> void serialize(Archive& archive, glm::vec4& v) { archive(v.x, v.y, v.z, v.w); }
@@ -19,17 +19,13 @@ namespace glm
 	template<class Archive> void serialize(Archive& archive, glm::dvec2& v) { archive(v.x, v.y); }
 	template<class Archive> void serialize(Archive& archive, glm::dvec3& v) { archive(v.x, v.y, v.z); }
 	template<class Archive> void serialize(Archive& archive, glm::dvec4& v) { archive(v.x, v.y, v.z, v.w); }
-
-	// glm matrices serialization
 	template<class Archive> void serialize(Archive& archive, glm::mat2& m) { archive(m[0], m[1]); }
 	template<class Archive> void serialize(Archive& archive, glm::dmat2& m) { archive(m[0], m[1]); }
 	template<class Archive> void serialize(Archive& archive, glm::mat3& m) { archive(m[0], m[1], m[2]); }
 	template<class Archive> void serialize(Archive& archive, glm::mat4& m) { archive(m[0], m[1], m[2], m[3]); }
 	template<class Archive> void serialize(Archive& archive, glm::dmat4& m) { archive(m[0], m[1], m[2], m[3]); }
-
-	//template<class Archive> void serialize(Archive& archive, glm::quat& q) { archive(q.x, q.y, q.z, q.w); }
-	//template<class Archive> void serialize(Archive& archive, glm::dquat& q) { archive(q.x, q.y, q.z, q.w); }
-
+	template<class Archive> void serialize(Archive& archive, glm::quat& q) { archive(q.x, q.y, q.z, q.w); }
+	template<class Archive> void serialize(Archive& archive, glm::dquat& q) { archive(q.x, q.y, q.z, q.w); }
 }
 
 struct TagComponent {
