01708df02532c97bd34e2b78324bee64ad577e2c
Uğur Güney <ugurguney@gmail.com>
2021-10-04 00:57:56 -0400

    DrawVec3Control for fancy vec3 manipulation UI with colored axes, drag floats and reset values.

--- Editor/Panels/InspectorPanel.cpp
+++ Editor/Panels/InspectorPanel.cpp
@@ -4,8 +4,62 @@
 
 #include <imgui.h>
 #include <imgui_stdlib.h>
+#include <imgui_internal.h> // for PushMultiItemsWidths, GImGui
 #include <entt/entt.hpp>
 #include <glm/gtc/type_ptr.hpp>
+
+static void DrawVec3Control(const std::string& label, glm::vec3& values, float resetValue = 0.0f, float columnWidth = 100.0f) {
+	ImGui::PushID(label.c_str());
+
+	ImGui::Columns(2);
+	ImGui::SetColumnWidth(0, columnWidth);
+	ImGui::Text(label.c_str());
+	ImGui::NextColumn();
+
+	ImGui::PushMultiItemsWidths(3, ImGui::CalcItemWidth());
+	ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2{ 0, 0 });
+
+	float lineHeight = GImGui->Font->FontSize + GImGui->Style.FramePadding.y * 2.0f;
+	ImVec2 buttonSize = { lineHeight + 3.0f, lineHeight };
+
+	// https://coolors.co/ff595e-ffca3a-8ac926-1982c4-6a4c93
+	ImGui::PushStyleColor(ImGuiCol_Button, ImVec4{ 1.0f, 0.34901961f, 0.36862745f, 1.0f });
+	ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4{ 1.000000f, 0.521569f, 0.537255f, 1.0f });
+	ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4{ 1.000000f, 0.200000f, 0.227451f, 1.0f });
+
+	if (ImGui::Button("X", buttonSize)) { values.x = resetValue; }
+	ImGui::PopStyleColor(3);
+
+	ImGui::SameLine();
+	ImGui::DragFloat("##X", &values.x, 0.1f, 0.0f, 0.0f, "%.2f");
+	ImGui::PopItemWidth();
+	ImGui::SameLine();
+
+	ImGui::PushStyleColor(ImGuiCol_Button, ImVec4{ 0.541176f, 0.788235f, 0.149020f, 1.0f });
+	ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4{ 0.631373f, 0.858824f, 0.262745f, 1.0f });
+	ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4{ 0.462745f, 0.670588f, 0.129412f, 1.0f });
+	if (ImGui::Button("Y", buttonSize)) { values.y = resetValue; }
+	ImGui::PopStyleColor(3);
+
+	ImGui::SameLine();
+	ImGui::DragFloat("##Y", &values.y, 0.1f, 0.0f, 0.0f, "%.2f");
+	ImGui::PopItemWidth();
+	ImGui::SameLine();
+
+	ImGui::PushStyleColor(ImGuiCol_Button, ImVec4{ 0.098039f, 0.509804f, 0.768627f, 1.0f });
+	ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4{ 0.149020f, 0.607843f, 0.890196f, 1.0f });
+	ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4{ 0.082353f, 0.423529f, 0.635294f, 1.0f });
+	if (ImGui::Button("Z", buttonSize)) { values.z = resetValue; }
+	ImGui::PopStyleColor(3);
+
+	ImGui::SameLine();
+	ImGui::DragFloat("##Z", &values.z, 0.1f, 0.0f, 0.0f, "%.2f");
+	ImGui::PopItemWidth();
+
+	ImGui::PopStyleVar();
+	ImGui::Columns(1);
+	ImGui::PopID();
+}
 
 void InspectorPanel::OnImGuiRender() {
 	ImGui::Begin("Inspector");
@@ -18,9 +72,9 @@ void InspectorPanel::OnImGuiRender() {
 			if (info == entt::type_id<TransformComponent>()) {
 				if (ImGui::CollapsingHeader("Transform", ImGuiTreeNodeFlags_DefaultOpen)) {
 					auto& transform = selectedObject.get<TransformComponent>();
-					ImGui::InputFloat3("Translation", glm::value_ptr(transform.translation));
-					ImGui::InputFloat3("Rotation", glm::value_ptr(transform.rotation));
-					ImGui::InputFloat3("Scale", glm::value_ptr(transform.scale));
+					DrawVec3Control("Translation", transform.translation);
+					DrawVec3Control("Rotation", transform.rotation);
+					DrawVec3Control("Scale", transform.scale, 1.0f);
 				}
 			}
 			else if (info == entt::type_id<MeshComponent>()) {
