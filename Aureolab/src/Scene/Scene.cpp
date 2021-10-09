0b5df1cdbb307dfd789227919f648254be816f2d
Uğur Güney <ugurguney@gmail.com>
2021-10-09 00:15:34 -0400

    Let ProceduralMeshComponent's be serialized with the scene.

--- Aureolab/src/Scene/Scene.cpp
+++ Aureolab/src/Scene/Scene.cpp
@@ -39,14 +39,14 @@ void Scene::New() {
 void Scene::SaveToFile(const std::string& filepath) {
 	std::ofstream file(filepath);
 	cereal::JSONOutputArchive output{ file };
-	entt::snapshot{ registry }.entities(output).component<TagComponent, TransformComponent, MeshComponent, MeshRendererComponent>(output);
+	entt::snapshot{ registry }.entities(output).component<TagComponent, TransformComponent, MeshComponent, ProceduralMeshComponent, MeshRendererComponent>(output);
 }
 
 void Scene::LoadFromFile(const std::string& filepath) {
 	registry.clear();
 	std::ifstream file(filepath);
 	cereal::JSONInputArchive input{ file };
-	entt::snapshot_loader{ registry }.entities(input).component<TagComponent, TransformComponent, MeshComponent, MeshRendererComponent>(input).orphans();
+	entt::snapshot_loader{ registry }.entities(input).component<TagComponent, TransformComponent, MeshComponent, ProceduralMeshComponent, MeshRendererComponent>(input).orphans();
 }
 
 void Scene::SaveToMemory() {
@@ -55,13 +55,13 @@ void Scene::SaveToMemory() {
 	storage.clear();
 
 	cereal::JSONOutputArchive outputMemory{ storage };
-	entt::snapshot{ registry }.entities(outputMemory).component<TagComponent, TransformComponent, MeshComponent, MeshRendererComponent>(outputMemory);
+	entt::snapshot{ registry }.entities(outputMemory).component<TagComponent, TransformComponent, MeshComponent, ProceduralMeshComponent, MeshRendererComponent>(outputMemory);
 }
 
 void Scene::LoadFromMemory() {
 	cereal::JSONInputArchive input{ storage };
 	registry.clear();
-	entt::snapshot_loader{ registry }.entities(input).component<TagComponent, TransformComponent, MeshComponent, MeshRendererComponent>(input).orphans();
+	entt::snapshot_loader{ registry }.entities(input).component<TagComponent, TransformComponent, MeshComponent, ProceduralMeshComponent, MeshRendererComponent>(input).orphans();
 	// rewind storage
 	storage.clear();
 	storage.seekg(0, std::ios_base::beg);
