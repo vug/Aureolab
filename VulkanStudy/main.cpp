807c09621e4d656155a288b64d8a68884cd147a8
Uğur Güney <ugurguney@gmail.com>
2021-10-28 00:02:04 -0400

    Start a GLFW window without any context.

--- VulkanStudy/main.cpp
+++ VulkanStudy/main.cpp
@@ -1,28 +1,51 @@
-#include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+// Looks like GLFW comes with Vulkan headers. No need for: #include <vulkan/vulkan.h>
 
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
 
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
 class HelloTriangleApplication {
 public:
     void run() {
+        initWindow();
         initVulkan();
         mainLoop();
         cleanup();
     }
 
 private:
+    GLFWwindow* window;
+
+    void initWindow() {
+        glfwInit();
+
+        // Do not create OpenGL context.
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        // Resizing windows will require special case. Will handle later.
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+    }
+
     void initVulkan() {
 
     }
 
     void mainLoop() {
-
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
     }
 
     void cleanup() {
+        glfwDestroyWindow(window);
 
+        glfwTerminate();
     }
 };
 
