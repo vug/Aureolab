37f4956b9397c6cb383386de76ca7aca96a6eed9
Uğur Güney <ugurguney@gmail.com>
2021-10-28 00:32:40 -0400

    Create a Vulkan Instance
    * Feed ApplicationInfo, CreateInfo structs to CreateInstance function.
    * List extensions required by GLFW and list all available extensions.
    * destroy instance while shutting down.

--- VulkanStudy/main.cpp
+++ VulkanStudy/main.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <vector>
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
@@ -21,6 +22,8 @@ public:
 private:
     GLFWwindow* window;
 
+    VkInstance instance;
+
     void initWindow() {
         glfwInit();
 
@@ -33,7 +36,8 @@ private:
     }
 
     void initVulkan() {
-
+        createInstance();
+        listExtensions();
     }
 
     void mainLoop() {
@@ -43,9 +47,58 @@ private:
     }
 
     void cleanup() {
+        vkDestroyInstance(instance, nullptr);
+
         glfwDestroyWindow(window);
 
         glfwTerminate();
+    }
+
+    void createInstance() {
+        // https://khronos.org/registry/vulkan/specs/1.2-extensions/html/chap4.html#VkApplicationInfo
+        VkApplicationInfo appInfo{}; // value initialization leaves un-set values as nullptr
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_2;
+
+        // https://khronos.org/registry/vulkan/specs/1.2-extensions/html/chap4.html#VkInstanceCreateInfo
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+        // Vulkan extensions required by GLFW
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        // Will enable "global validation layers" later
+        createInfo.enabledLayerCount = 0;
+
+        std::cout << "glfw required extensions:" << std::endl;
+        for (int i = 0; i < glfwExtensionCount; i++) {
+            std::cout << "\t" << glfwExtensions[i] << std::endl;
+        }
+
+        // https://khronos.org/registry/vulkan/specs/1.2-extensions/html/chap4.html#vkCreateInstance
+        // Creation Parameters, Allocator Callbacks, Created/Returned Object
+        if (vkCreateInstance(&createInfo, nullptr, &instance)) { // returns VkResult 
+            throw std::runtime_error("failed to create Vulkan instance!");
+        }
+    }
+
+    void listExtensions() {
+        uint32_t extensionCount = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+        std::vector<VkExtensionProperties> extensions(extensionCount);
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+        
+        std::cout << "available Vulkan extensions:\n";
+        for (const auto& extension : extensions) {
+            std::cout << '\t' << extension.extensionName << '\n';
+        }
     }
 };
 
