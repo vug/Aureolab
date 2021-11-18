d42d31984a66c855a4fe06ad3686bf4ce076f85e
Uğur Güney <ugurguney@gmail.com>
2021-11-18 03:29:08 -0500

    [vulkan-renderer] example with 2 pipelines with different shaders
    * alternate between them

--- VulkanRenderer/main.cpp
+++ VulkanRenderer/main.cpp
@@ -17,11 +17,13 @@ int main() {
         presentFramebuffers[i] = vr.CreateFramebuffer(renderPass, presentImageViews[i], vc.GetSwapchainInfo().extent);
     }
 
-    auto vertShaderByteCode = vr.ReadFile("assets/shaders/example-vert.spv");
-    auto fragShaderByteCode = vr.ReadFile("assets/shaders/example-frag.spv");
-    VkShaderModule vertShaderModule = vr.CreateShaderModule(vertShaderByteCode);
-    VkShaderModule fragShaderModule = vr.CreateShaderModule(fragShaderByteCode);
-    auto [pipeline, pipelineLayout] = vr.CreateSinglePassGraphicsPipeline(vertShaderModule, fragShaderModule, renderPass);
+    VkShaderModule vertShader1 = vr.CreateShaderModule(vr.ReadFile("assets/shaders/example-triangle-vert.spv"));
+    VkShaderModule fragShader1 = vr.CreateShaderModule(vr.ReadFile("assets/shaders/example-triangle-frag.spv"));
+    auto [pipeline1, pipelineLayout1] = vr.CreateSinglePassGraphicsPipeline(vertShader1, fragShader1, renderPass);
+
+    VkShaderModule vertShader2 = vr.CreateShaderModule(vr.ReadFile("assets/shaders/example-square-vert.spv"));
+    VkShaderModule fragShader2 = vr.CreateShaderModule(vr.ReadFile("assets/shaders/example-square-frag.spv"));
+    auto [pipeline2, pipelineLayout2] = vr.CreateSinglePassGraphicsPipeline(vertShader2, fragShader2, renderPass);
 
     while (!win.ShouldClose()) {
         win.PollEvents();
@@ -30,10 +32,15 @@ int main() {
         static int frameNumber = 0;
         float flash = abs(sin(frameNumber / 2400.f));
         clearValue.color = { { 0.0f, 0.0f, flash, 1.0f } };
-        vc.drawFrameBlocked(renderPass, cmdBuf, presentFramebuffers, vc.GetSwapchainInfo(), clearValue,
-            [&](VkCommandBuffer& cmd) {
-            vkCmdBindPipeline(cmd, VK_PIPELINE_BIND_POINT_GRAPHICS, pipeline);
-            vkCmdDraw(cmd, 3, 1, 0, 0);
+        vc.drawFrameBlocked(renderPass, cmdBuf, presentFramebuffers, vc.GetSwapchainInfo(), clearValue, [&](VkCommandBuffer& cmd) {
+            if (int(frameNumber / 4000.0f) % 2 == 0) {
+                vkCmdBindPipeline(cmd, VK_PIPELINE_BIND_POINT_GRAPHICS, pipeline1);
+                vkCmdDraw(cmd, 3, 1, 0, 0);
+            }
+            else {
+                vkCmdBindPipeline(cmd, VK_PIPELINE_BIND_POINT_GRAPHICS, pipeline2);
+                vkCmdDraw(cmd, 6, 1, 0, 0);
+            }
         });
 
         frameNumber++;
@@ -43,10 +50,15 @@ int main() {
     // should finish them before starting cleanup while leaving the main loop
     vkDeviceWaitIdle(vc.GetDevice());
 
-    vkDestroyShaderModule(vc.GetDevice(), vertShaderModule, nullptr);
-    vkDestroyShaderModule(vc.GetDevice(), fragShaderModule, nullptr);
-    vkDestroyPipelineLayout(vc.GetDevice(), pipelineLayout, nullptr);
-    vkDestroyPipeline(vc.GetDevice(), pipeline, nullptr);
+    vkDestroyShaderModule(vc.GetDevice(), vertShader1, nullptr);
+    vkDestroyShaderModule(vc.GetDevice(), fragShader1, nullptr);
+    vkDestroyPipelineLayout(vc.GetDevice(), pipelineLayout1, nullptr);
+    vkDestroyPipeline(vc.GetDevice(), pipeline1, nullptr);
+
+    vkDestroyShaderModule(vc.GetDevice(), vertShader2, nullptr);
+    vkDestroyShaderModule(vc.GetDevice(), fragShader2, nullptr);
+    vkDestroyPipelineLayout(vc.GetDevice(), pipelineLayout2, nullptr);
+    vkDestroyPipeline(vc.GetDevice(), pipeline2, nullptr);
 
     vkFreeCommandBuffers(vc.GetDevice(), vc.GetCommandPool(), 1, &cmdBuf);
     for (size_t i = 0; i < presentImageViews.size(); i++) {
