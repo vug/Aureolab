39216edcd9eb7db4d40660a43144b9e017ad7a6d
Uğur Güney <ugurguney@gmail.com>
2021-11-27 19:28:58 -0500

    VulkanDestroyer can destroy AllocatedBuffer

--- VulkanRenderer/VulkanDestroyer.h
+++ VulkanRenderer/VulkanDestroyer.h
@@ -1,23 +1,28 @@
 #pragma once
+#include "Types.h"
+
 #include <vulkan/vulkan.h>
+#include <vk_mem_alloc.h>
 
 #include <variant>
 #include <vector>
 
 class VulkanDestroyer {
-    using VulkanObject = std::variant<VkRenderPass, VkFramebuffer, VkPipeline, VkPipelineLayout, VkShaderModule>;
+    using VulkanObject = std::variant<VkRenderPass, VkFramebuffer, VkPipeline, VkPipelineLayout, VkShaderModule, AllocatedBuffer>;
     struct Destroy {
-        Destroy(const VkDevice& dev) : device(dev) {}
+        Destroy(const VkDevice& dev, const VmaAllocator& alloc) : device(dev), allocator(alloc) {}
         void operator()(VkRenderPass obj) { vkDestroyRenderPass(device, obj, nullptr); }
         void operator()(VkFramebuffer obj) { vkDestroyFramebuffer(device, obj, nullptr); }
         void operator()(VkPipeline obj) { vkDestroyPipeline(device, obj, nullptr); }
         void operator()(VkPipelineLayout obj) { vkDestroyPipelineLayout(device, obj, nullptr); }
         void operator()(VkShaderModule obj) { vkDestroyShaderModule(device, obj, nullptr); }
+        void operator()(AllocatedBuffer obj) { vmaDestroyBuffer(allocator, obj.buffer, obj.allocation); }
     private:
         const VkDevice& device;
+        const VmaAllocator& allocator;
     };
 public:
-    VulkanDestroyer(const VkDevice& device) : destroy(device) {}
+    VulkanDestroyer(const VkDevice& device, const VmaAllocator& allocator) : destroy(device, allocator) {}
     void Add(VulkanObject obj) { objects.push_back(obj); }
     template <typename T>
     void Add(std::vector<T> objects) { 
