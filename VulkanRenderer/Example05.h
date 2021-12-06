42f8d77dad0fee5897b57bfca534af67ba357abd
Uğur Güney <ugurguney@gmail.com>
2021-12-06 01:07:17 -0500

    Load MineCraft model and texture. Have a second instance of textured material to use this second texture.
    * And rotate them in different angular velocities.

--- VulkanRenderer/Example05.h
+++ VulkanRenderer/Example05.h
@@ -24,16 +24,22 @@ public:
             mesh.MakeTriangle();
             vr.UploadMesh(mesh);
             vr.meshes["triangle"] = mesh; // copy operation
+            destroyer.Add(vr.meshes["triangle"].vertexBuffer);
 
             mesh.MakeQuad();
             vr.UploadMesh(mesh);
             vr.meshes["quad"] = mesh;
+            destroyer.Add(vr.meshes["quad"].vertexBuffer);
 
             mesh.LoadFromOBJ("assets/models/suzanne.obj");
             vr.UploadMesh(mesh);
             vr.meshes["monkey_flat"] = mesh;
+            destroyer.Add(vr.meshes["monkey_flat"].vertexBuffer);
 
-            destroyer.Add(std::vector{ vr.meshes["triangle"].vertexBuffer, vr.meshes["quad"].vertexBuffer, vr.meshes["monkey_flat"].vertexBuffer });
+            mesh.LoadFromOBJ("assets/models/lost_empire.obj");
+            vr.UploadMesh(mesh);
+            vr.meshes["lost_empire"] = mesh;
+            destroyer.Add(vr.meshes["lost_empire"].vertexBuffer);
         }
 
         VkDescriptorSetLayout singleTextureSetLayout;
@@ -59,10 +65,15 @@ public:
             texture.LoadImageFromFile("assets/textures/texture.jpg");
             vr.UploadTexture(texture);
             vr.textures["sculpture"] = texture;
-
-            destroyer.Add(texture.imageView);
+            destroyer.Add(vr.textures["sculpture"].imageView);
             // TODO figure out how to add image to deletion queue from UploadTexture. Observe how texture's and newImage's addresses change etc.
-            destroyer.Add(texture.newImage);
+            destroyer.Add(vr.textures["sculpture"].newImage);
+
+            texture.LoadImageFromFile("assets/textures/lost_empire-RGBA.png");
+            vr.UploadTexture(texture);
+            vr.textures["lost_empire"] = texture;
+            destroyer.Add(vr.textures["lost_empire"].imageView);
+            destroyer.Add(vr.textures["lost_empire"].newImage);
         }
 
         // Samplers
@@ -141,43 +152,79 @@ public:
             destroyer.Add(pipelineLayout);
             destroyer.Add(pipeline);
 
-            vertShader = vr.CreateShaderModule(vr.ReadFile("assets/shaders/example-05-textured-vert.spv"));
-            fragShader = vr.CreateShaderModule(vr.ReadFile("assets/shaders/example-05-textured-frag.spv"));
-            std::tie(pipeline, pipelineLayout) = vr.CreateSinglePassGraphicsPipeline(vertShader, fragShader, Vertex::GetVertexDescription(), MeshPushConstants::GetPushConstantRanges(), texturedMaterialSetLayouts, renderPass);
-            vr.materials["textured"] = Material{ pipeline, pipelineLayout };
+            // Textured Material 1
+            {
+                vertShader = vr.CreateShaderModule(vr.ReadFile("assets/shaders/example-05-textured-vert.spv"));
+                fragShader = vr.CreateShaderModule(vr.ReadFile("assets/shaders/example-05-textured-frag.spv"));
+                std::tie(pipeline, pipelineLayout) = vr.CreateSinglePassGraphicsPipeline(vertShader, fragShader, Vertex::GetVertexDescription(), MeshPushConstants::GetPushConstantRanges(), texturedMaterialSetLayouts, renderPass);
+                vr.materials["textured"] = Material{ pipeline, pipelineLayout };
+                destroyer.Add(std::vector{ vertShader, fragShader });
+                destroyer.Add(pipelineLayout);
+                destroyer.Add(pipeline);
 
-            // TODO: Hide descriptor set image write stuff behind an abstraction
-            // TODO: Also an abstraction for combining varius descriptor sets in various shaders
-            VkDescriptorSetAllocateInfo allocInfo = {};
-            allocInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO;
-            allocInfo.descriptorPool = descriptorPool;
-            allocInfo.descriptorSetCount = 1;
-            allocInfo.pSetLayouts = &singleTextureSetLayout;
-            vkAllocateDescriptorSets(vc.GetDevice(), &allocInfo, &vr.materials["textured"].textureSet);
+                // TODO: Hide descriptor set image write stuff behind an abstraction
+                // TODO: Also an abstraction for combining varius descriptor sets in various shaders
+                VkDescriptorSetAllocateInfo allocInfo = {};
+                allocInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO;
+                allocInfo.descriptorPool = descriptorPool;
+                allocInfo.descriptorSetCount = 1;
+                allocInfo.pSetLayouts = &singleTextureSetLayout;
+                vkAllocateDescriptorSets(vc.GetDevice(), &allocInfo, &vr.materials["textured"].textureSet);
 
-            //write to the descriptor set so that it points to our empire_diffuse texture
-            VkDescriptorImageInfo imageBufferInfo;
-            imageBufferInfo.sampler = blockySampler;
-            imageBufferInfo.imageView = vr.textures["sculpture"].imageView;
-            imageBufferInfo.imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
+                // Write to the descriptor set so that it points to given texture
+                VkDescriptorImageInfo imageBufferInfo;
+                imageBufferInfo.sampler = blockySampler;
+                imageBufferInfo.imageView = vr.textures["sculpture"].imageView;
+                imageBufferInfo.imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
 
-            VkWriteDescriptorSet writeSetTexture = {};
-            writeSetTexture.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
-            writeSetTexture.dstBinding = 0;
-            writeSetTexture.dstSet = vr.materials["textured"].textureSet;
-            writeSetTexture.descriptorCount = 1;
-            writeSetTexture.descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
-            writeSetTexture.pImageInfo = &imageBufferInfo;
-            vkUpdateDescriptorSets(vc.GetDevice(), 1, &writeSetTexture, 0, nullptr);
+                VkWriteDescriptorSet writeSetTexture = {};
+                writeSetTexture.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+                writeSetTexture.dstBinding = 0;
+                writeSetTexture.dstSet = vr.materials["textured"].textureSet;
+                writeSetTexture.descriptorCount = 1;
+                writeSetTexture.descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
+                writeSetTexture.pImageInfo = &imageBufferInfo;
+                vkUpdateDescriptorSets(vc.GetDevice(), 1, &writeSetTexture, 0, nullptr);
+            }
 
-            destroyer.Add(std::vector{ vertShader, fragShader });
-            destroyer.Add(pipelineLayout);
-            destroyer.Add(pipeline);
+            // Textured Material 2
+            {
+                vertShader = vr.CreateShaderModule(vr.ReadFile("assets/shaders/example-05-textured-vert.spv"));
+                fragShader = vr.CreateShaderModule(vr.ReadFile("assets/shaders/example-05-textured-frag.spv"));
+                std::tie(pipeline, pipelineLayout) = vr.CreateSinglePassGraphicsPipeline(vertShader, fragShader, Vertex::GetVertexDescription(), MeshPushConstants::GetPushConstantRanges(), texturedMaterialSetLayouts, renderPass);
+                vr.materials["textured-lost_empire"] = Material{ pipeline, pipelineLayout };
+                destroyer.Add(std::vector{ vertShader, fragShader });
+                destroyer.Add(pipelineLayout);
+                destroyer.Add(pipeline);
+
+                VkDescriptorSetAllocateInfo allocInfo = {};
+                allocInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO;
+                allocInfo.descriptorPool = descriptorPool;
+                allocInfo.descriptorSetCount = 1;
+                allocInfo.pSetLayouts = &singleTextureSetLayout;
+                vkAllocateDescriptorSets(vc.GetDevice(), &allocInfo, &vr.materials["textured-lost_empire"].textureSet);
+
+                // Write to the descriptor set so that it points to given texture
+                VkDescriptorImageInfo imageBufferInfo;
+                imageBufferInfo.sampler = blockySampler;
+                imageBufferInfo.imageView = vr.textures["lost_empire"].imageView;
+                imageBufferInfo.imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
+
+                VkWriteDescriptorSet writeSetTexture = {};
+                writeSetTexture.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+                writeSetTexture.dstBinding = 0;
+                writeSetTexture.dstSet = vr.materials["textured-lost_empire"].textureSet;
+                writeSetTexture.descriptorCount = 1;
+                writeSetTexture.descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
+                writeSetTexture.pImageInfo = &imageBufferInfo;
+                vkUpdateDescriptorSets(vc.GetDevice(), 1, &writeSetTexture, 0, nullptr);
+            }
         }
 
         objects = {
-            { &vr.meshes["monkey_flat"], &vr.materials["vizNormal"], glm::translate(glm::mat4(1.0f), { -1.0f, 0.0, 0.0 }) },
+            { &vr.meshes["monkey_flat"], &vr.materials["vizNormal"], glm::translate(glm::scale(glm::mat4(1.0f), {0.5, 0.5, 0.5}), { -1.0f, 0.0, 0.0 }) },
             { &vr.meshes["quad"], &vr.materials["textured"], glm::translate(glm::mat4(1.0f), { 1.0f, 0.0, 0.0 }) },
+            { &vr.meshes["lost_empire"], &vr.materials["textured-lost_empire"], glm::translate(glm::mat4(1.0f), { 0.0f, -20.0, 0.0 }) },
         };
     }
 
@@ -194,8 +241,11 @@ public:
 
         glm::vec3 camPos = { 0.f, 0.f, -2.f };
 
-        for (auto& obj : objects) {
-            obj.transform = glm::rotate(obj.transform, delta.count(), { 0, 1, 0 });
+        static std::vector<float> angularVelocities = { 1.0f, 0.5f, 0.1f };
+        for (int i = 0; i < objects.size(); i++) {
+            auto& obj = objects[i];
+            float w = angularVelocities[i];
+            obj.transform = glm::rotate(obj.transform, delta.count() * w, { 0, 1, 0 });
         }
 
         vc.drawFrame(vc.GetDevice(), vc.GetSwapchain(), vc.GetGraphicsQueue(), renderPass, frameDatas, presentFramebuffers, vc.GetSwapchainInfo(), clearValues, [&](const VkCommandBuffer& cmd, uint32_t frameNo) {
