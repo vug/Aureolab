d02ff541faa3387f7b73fcfeb0e3473476d4243a
Uğur Güney <ugurguney@gmail.com>
2021-12-29 16:47:16 -0500

    [plain example] Create functions for Pipeline and PipelineLayout w/ VkPolygonMode and VkDynamicState parameters

--- VulkanRenderer/Examples/Example06.h
+++ VulkanRenderer/Examples/Example06.h
@@ -3,6 +3,210 @@
 
 #include <glm/gtx/transform.hpp>
 
+VkPipelineLayout CreatePipelineLayout(
+    const VulkanContext& vc,
+    const std::vector<VkPushConstantRange>& pushConstantRanges,
+    const std::vector<VkDescriptorSetLayout>& descSetLayouts) {
+    Log::Debug("\tCreating Pipeline Layout...");
+    VkPipelineLayout pipelineLayout;
+    VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
+    pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+    pipelineLayoutInfo.pushConstantRangeCount = static_cast<uint32_t>(pushConstantRanges.size());
+    pipelineLayoutInfo.pPushConstantRanges = pushConstantRanges.data();
+    pipelineLayoutInfo.setLayoutCount = static_cast<uint32_t>(descSetLayouts.size());
+    pipelineLayoutInfo.pSetLayouts = descSetLayouts.data();
+
+    if (vkCreatePipelineLayout(vc.GetDevice(), &pipelineLayoutInfo, nullptr, &pipelineLayout) != VK_SUCCESS) {
+        Log::Critical("failed to create pipeline layout!");
+        exit(EXIT_FAILURE);
+    }
+
+    vc.destroyer->Add(pipelineLayout);
+    return pipelineLayout;
+}
+
+VkPipeline CreatePipeline(
+    const VulkanContext& vc,
+    VkShaderModule vertShaderModule, VkShaderModule fragShaderModule,
+    const VertexInputDescription& vertDesc,
+    VkPipelineLayout pipelineLayout,
+    VkRenderPass renderPass,
+    VkPolygonMode polygonMode,
+    const std::vector<VkDynamicState>& dynamicStates
+) {
+    Log::Debug("Creating Graphics Pipeline...");
+
+    Log::Debug("\tCreating Shader Stage Info...");
+    VkPipelineShaderStageCreateInfo vertShaderStageInfo{};
+    vertShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+    vertShaderStageInfo.stage = VK_SHADER_STAGE_VERTEX_BIT;
+    vertShaderStageInfo.module = vertShaderModule;
+    // Entrypoint. can combine multiple shaders into a single file, each of which having a different entrypoint.
+    vertShaderStageInfo.pName = "main";
+    // To set shader constants to help compiler (more efficient than doing it at render time)
+    vertShaderStageInfo.pSpecializationInfo = nullptr;
+
+    VkPipelineShaderStageCreateInfo fragShaderStageInfo{};
+    fragShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+    fragShaderStageInfo.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+    fragShaderStageInfo.module = fragShaderModule;
+    fragShaderStageInfo.pName = "main";
+
+    VkPipelineShaderStageCreateInfo shaderStages[] = { vertShaderStageInfo, fragShaderStageInfo };
+
+    Log::Debug("\tVertex Input State Info...");
+    VkPipelineVertexInputStateCreateInfo vertexInputInfo{};
+    vertexInputInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
+    vertexInputInfo.vertexBindingDescriptionCount = static_cast<uint32_t>(vertDesc.bindings.size());
+    vertexInputInfo.pVertexBindingDescriptions = vertDesc.bindings.data();
+    vertexInputInfo.vertexAttributeDescriptionCount = static_cast<uint32_t>(vertDesc.attributes.size());
+    vertexInputInfo.pVertexAttributeDescriptions = vertDesc.attributes.data();
+
+    Log::Debug("\tInput Assembly State Info...");
+    VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo{};
+    inputAssemblyInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
+    // There are also POINT_LIST, LINE_LIST, TRIANGLE_STRIP etc.
+    inputAssemblyInfo.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
+    // restart is useful in instance rendering
+    inputAssemblyInfo.primitiveRestartEnable = VK_FALSE;
+
+    Log::Debug("\tNo Tesselation State Info.");
+
+    Log::Debug("\tViewport State Info...");
+    VkViewport viewport{};
+    viewport.x = 0.0f;
+    viewport.y = 0.0f;
+    //viewport.width = (float)vc.GetSwapchainInfo().extent.width / 3;
+    viewport.width = (float)vc.GetSwapchainInfo().extent.width;
+    viewport.height = (float)vc.GetSwapchainInfo().extent.height;
+    viewport.minDepth = 0.0f;
+    viewport.maxDepth = 1.0f;
+    VkRect2D scissor{};
+    scissor.offset = { 0, 0 };
+    scissor.extent = vc.GetSwapchainInfo().extent;
+    //scissor.extent.width /= 3;
+    VkPipelineViewportStateCreateInfo viewportStateInfo{};
+    viewportStateInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
+    viewportStateInfo.viewportCount = 1;
+    viewportStateInfo.pViewports = &viewport;
+    viewportStateInfo.scissorCount = 1;
+    viewportStateInfo.pScissors = &scissor;
+
+    Log::Debug("\tRasterization State Info...");
+    VkPipelineRasterizationStateCreateInfo rasterizerInfo{};
+    rasterizerInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
+    // when true clamps fragments nearer than near plane or further than far plane
+    // as opposed to discarding (might be useful for shadow maps)
+    rasterizerInfo.depthClampEnable = VK_FALSE;
+    // when tru geometry never passes rasterizer stage. => disables framebuffer.
+    rasterizerInfo.rasterizerDiscardEnable = VK_FALSE;
+    // Triangle rendering mode. {FILL, LINE, POINT}. Other modes require a GPU feature.
+    rasterizerInfo.polygonMode = polygonMode;
+    // NONE, FRONT, BACK, or FRONT_AND_BACK
+    rasterizerInfo.cullMode = VK_CULL_MODE_NONE;
+    //rasterizerInfo.cullMode = VK_CULL_MODE_BACK_BIT;
+    // or clock-wise (we did y-flip, hence need to switch from CW to CCW)
+    rasterizerInfo.frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE;
+    rasterizerInfo.depthBiasEnable = VK_FALSE;
+    // contant depth added to each fragment
+    rasterizerInfo.depthBiasConstantFactor = 0.0f;
+    rasterizerInfo.depthBiasClamp = 0.0f;
+    rasterizerInfo.depthBiasSlopeFactor = 0.0f;
+    // max value depends on hardward. lw > 1 requires wideLines GPU feature.
+    rasterizerInfo.lineWidth = 1.0f;
+
+    Log::Debug("\tMultisample State Info...");
+    VkPipelineMultisampleStateCreateInfo multisamplingInfo{};
+    multisamplingInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
+    // number of samples per fragment
+    multisamplingInfo.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
+    multisamplingInfo.sampleShadingEnable = VK_FALSE;
+    multisamplingInfo.minSampleShading = 1.0f;
+    multisamplingInfo.pSampleMask = nullptr;
+    // Might be useful later.
+    multisamplingInfo.alphaToCoverageEnable = VK_FALSE;
+    multisamplingInfo.alphaToOneEnable = VK_FALSE;
+
+    Log::Debug("\tDepth Stencil State Info...");
+    VkPipelineDepthStencilStateCreateInfo depthStencilInfo{};
+    depthStencilInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_DEPTH_STENCIL_STATE_CREATE_INFO;
+    depthStencilInfo.pNext = nullptr;
+    depthStencilInfo.depthTestEnable = VK_TRUE;
+    depthStencilInfo.depthWriteEnable = VK_TRUE;
+    depthStencilInfo.depthCompareOp = VK_COMPARE_OP_LESS_OR_EQUAL;
+    depthStencilInfo.depthBoundsTestEnable = VK_FALSE;
+    depthStencilInfo.minDepthBounds = 0.0f; // Optional
+    depthStencilInfo.maxDepthBounds = 1.0f; // Optional
+    depthStencilInfo.stencilTestEnable = VK_FALSE;
+
+    Log::Debug("\tColor Blend State Info...");
+    // About combining fragment shader color with the color already in Framebuffer
+    // Can be done via colorBlendOp
+    VkPipelineColorBlendAttachmentState colorBlendAttachment{};
+    colorBlendAttachment.colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT;
+    // When false, new color from fragment shader is written into Framebuffer without modification. 
+    colorBlendAttachment.blendEnable = VK_FALSE;
+
+    // Example
+    VkPipelineColorBlendAttachmentState standardAlphaBlending{};
+    standardAlphaBlending.colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT;
+    standardAlphaBlending.blendEnable = VK_TRUE;
+    standardAlphaBlending.srcColorBlendFactor = VK_BLEND_FACTOR_SRC_ALPHA;
+    standardAlphaBlending.dstColorBlendFactor = VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA;
+    standardAlphaBlending.colorBlendOp = VK_BLEND_OP_ADD;
+    standardAlphaBlending.srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE;
+    standardAlphaBlending.dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO;
+    standardAlphaBlending.alphaBlendOp = VK_BLEND_OP_ADD;
+
+    VkPipelineColorBlendStateCreateInfo colorBlendingInfo{};
+    colorBlendingInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
+    // Blending also can be done via bitwise operations. Turning this one disables color blending, as if blendEnable was false
+    colorBlendingInfo.logicOpEnable = VK_FALSE;
+    //colorBlendingInfo.logicOp = VK_LOGIC_OP_COPY; // Optional
+    colorBlendingInfo.attachmentCount = 1;
+    colorBlendingInfo.pAttachments = &colorBlendAttachment;
+   
+    VkPipelineDynamicStateCreateInfo dynamicStateCreateInfo = {};
+    dynamicStateCreateInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_DYNAMIC_STATE_CREATE_INFO;
+    dynamicStateCreateInfo.dynamicStateCount = static_cast<uint32_t>(dynamicStates.size());
+    dynamicStateCreateInfo.pDynamicStates = dynamicStates.data();
+
+    Log::Debug("\tCreating Pipeline...");
+    // Made of 1) Shader stages, 2) Fixed-function states,
+    // 3) Pipeline Layout (uniform/push values used by shaders, updated at draw time), 4) Renderpass
+    VkGraphicsPipelineCreateInfo pipelineInfo{};
+    pipelineInfo.sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
+    // 1) Shaders
+    pipelineInfo.stageCount = 2;
+    pipelineInfo.pStages = shaderStages;
+    // 2) Fixed-function states
+    pipelineInfo.pVertexInputState = &vertexInputInfo;
+    pipelineInfo.pInputAssemblyState = &inputAssemblyInfo;
+    pipelineInfo.pViewportState = &viewportStateInfo;
+    pipelineInfo.pRasterizationState = &rasterizerInfo;
+    pipelineInfo.pMultisampleState = &multisamplingInfo;
+    pipelineInfo.pDepthStencilState = &depthStencilInfo;
+    pipelineInfo.pColorBlendState = &colorBlendingInfo;
+    pipelineInfo.pDynamicState = &dynamicStateCreateInfo;
+    // 3) Pipeline Layout
+    pipelineInfo.layout = pipelineLayout;
+    // 4) Renderpass
+    pipelineInfo.renderPass = renderPass;
+    pipelineInfo.subpass = 0;
+    // could have created (derive) this pipeline based on another pipeline
+    pipelineInfo.basePipelineHandle = VK_NULL_HANDLE; // Optional
+    pipelineInfo.basePipelineIndex = -1; // Optional
+
+    VkPipeline graphicsPipeline;
+    if (vkCreateGraphicsPipelines(vc.GetDevice(), VK_NULL_HANDLE, 1, &pipelineInfo, nullptr, &graphicsPipeline) != VK_SUCCESS) {
+        Log::Critical("failed to create graphics pipeline!");
+        exit(EXIT_FAILURE);
+    }
+    vc.destroyer->Add(graphicsPipeline);
+
+    return graphicsPipeline;
+}
+
 // Empty but running example with no abstractions for command generation, frames-in-flight handling, pipeline creation etc.
 class Ex06Plain : public Example {
 public:
